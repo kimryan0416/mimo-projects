e5eb02faf11b8972e0db91dc5e6dadc09eafbc46
Ryan Kim <kimryan0416@gmail.com>
2021-08-05 05:56:12 -0400

    added react router for navigation

--- src/App.js
+++ src/App.js
@@ -1,46 +1,68 @@
 import { Component } from "react";
-
+import {
+  BrowserRouter as Router,
+  withRouter,
+  Switch,
+  Link,
+  Route
+} from "react-router-dom";
 import './App.css';
+
 import ToDoList from "./to-do-list/ToDoList";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      page: 2
-    }
-    this.pages = [
-      {
-        title:"Photo App",
-        content:null
-      },
-      {
-        title:"Stopwatch",
-        content:null
-      },
-      {
-        title:"To-Do List",
-        content:(<ToDoList />)
-      },
-      {
-        title:"Minesweeper",
-        content:null
-      }
-    ]
   }
 
   render() {
-    var page = this.pages[this.state.page];
-    var content = (page) ? page.content : null;
-    var c = (page) ? "selected" : ""
     return (
-      <div className={`App ${c}`}>
-        {content}
+      <div className="App">
+        <Router>
+          <header>
+            <Link to="/">Back</Link>
+          </header>
+          <Switch>
+            <Route exact path="/photo" component={withRouter(Photo)} />
+            <Route exact path="/stopwatch" component={withRouter(Stopwatch)} />
+            <Route exact path="/todo" component={withRouter(ToDoList)} />
+            <Route exact path="/minesweeper" component={withRouter(Minesweeper)} />
+            <Route path="/" component={withRouter(Home)} />
+          </Switch>
+        </Router>
       </div>
     );
   }
 }
 
+const Home = () => {
+  return (
+    <>
+      <h1>Menu</h1>
+      <Link to="/photo">Photo App</Link>
+      <Link to="/stopwatch">Stopwatch</Link>
+      <Link to="/todo">To-Do App</Link>
+      <Link to="/minesweeper">Minesweeper</Link>
+    </>
+  );
+}
 
+const Photo = () => {
+  return (
+    <h1>Photo App</h1>
+  );
+}
+
+const Stopwatch = () => {
+  return (
+    <h1>Stopwatch</h1>
+  );
+}
+
+const Minesweeper = () => {
+  return (
+    <h1>Minesweeper</h1>
+  );
+}
 
 export default App;
